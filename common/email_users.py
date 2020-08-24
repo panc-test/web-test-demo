cc860a0d7b568aa8b527d9ef82743ecb05e45818
panc <34005363+panc-test@users.noreply.github.com>
2020-08-24 17:28:18 +0800

    Update email_users.py

--- common/email_users.py
+++ common/email_users.py
@@ -1,6 +1,6 @@
 
 
 # 邮件发送人和收件人信息
-send_eamil = '1976085712@qq.com'  # 发送方邮箱
-password = 'zwwycytjpmxcjfgh'  # 填入发送方邮箱的授权码
-receiver_mail = 'a1976085712@163.com'  # 收件人邮箱
+send_eamil = 'aaa@qq.com'  # 发送方邮箱
+password = 'xxx'  # 填入发送方邮箱的授权码
+receiver_mail = 'bbb@163.com'  # 收件人邮箱
